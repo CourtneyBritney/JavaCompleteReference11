ff59ea4f0a639ddad43958a0b863270b5a271cb6
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-30 11:26:23 +0200

    Added Files

--- Classes/src/com/courtney/Box.java
+++ Classes/src/com/courtney/Box.java
@@ -1,15 +1,14 @@
 package com.courtney;
 
-// This program includes a method inside the box class.
+// Now, volume() returns the volume of a box.
 
 class Box {
     double width;
     double height;
     double depth;
 
-    // display volume of a box
-    void volume() {
-        System.out.print("Volume is ");
-        System.out.println(width * height * depth);
+    // compute and return volume
+    double volume() {
+        return width * height * depth;
     }
 }
