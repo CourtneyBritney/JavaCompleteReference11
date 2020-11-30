cc0a5dc2f1be033bd858298fb4dc09c98790f463
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-30 14:30:21 +0200

    Added Files

---
+++ Inheritance/src/com/courtney/AB.java
@@ -0,0 +1,24 @@
+package com.courtney;
+
+// A simple example of inheritance.
+
+// Create a superclass.
+class A {
+    int i, j;
+
+    void showij() {
+        System.out.println("i and j: " + i + " " + j);
+    }
+}
+
+// Create a subclass by extending class A.
+class B extends A {
+    int k;
+
+    void showk() {
+        System.out.println("k: " + k);
+    }
+    void sum() {
+        System.out.println("i+j+k: " + (i+j+k));
+    }
+}
