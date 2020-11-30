5fc1b9e77ed10cab9db3103bb31d1ad1462feffa
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-30 11:24:58 +0200

    Added Files

--- Classes/src/com/courtney/Box.java
+++ Classes/src/com/courtney/Box.java
@@ -1,9 +1,15 @@
 package com.courtney;
 
-// This program declares two Box objects.
+// This program includes a method inside the box class.
 
 class Box {
     double width;
     double height;
     double depth;
+
+    // display volume of a box
+    void volume() {
+        System.out.print("Volume is ");
+        System.out.println(width * height * depth);
+    }
 }
