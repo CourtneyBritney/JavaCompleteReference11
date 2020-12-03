8bb2302c2df9d3f892ce4029d46a2626ef2f961a
CourtneyBritney <courtney02alexander@gmail.com>
2020-12-03 08:58:55 +0200

    Added Files

---
+++ PackagesAndInterfaces/src/com/courtney/Balance.java
@@ -0,0 +1,19 @@
+package com.courtney;
+
+// A simple package
+
+class Balance {
+    String name;
+    double bal;
+
+    Balance(String n, double b) {
+        name = n;
+        bal = b;
+    }
+
+    void show() {
+        if(bal<0)
+            System.out.print("-->> ");
+        System.out.println(name + ": $" + bal);
+    }
+}
