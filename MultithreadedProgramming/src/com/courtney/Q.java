2d020ec8d94f27339de4239e742ea7ffa6e02607
CourtneyBritney <courtney02alexander@gmail.com>
2020-12-09 13:16:30 +0200

    Added Files

---
+++ MultithreadedProgramming/src/com/courtney/Q.java
@@ -0,0 +1,16 @@
+package com.courtney;
+
+// An incorrect implementation of a producer and consumer.
+class Q {
+    int n;
+
+    synchronized int get() {
+        System.out.println("Got: " + n);
+        return n;
+    }
+
+    synchronized void put(int n) {
+        this.n = n;
+        System.out.println("Put: " + n);
+    }
+}
