94e0cca1a1b72d5ffa49010d1115019a71148ccd
CourtneyBritney <courtney02alexander@gmail.com>
2020-12-14 13:23:20 +0200

    Added Files

--- EnumsAutoboxingAnnotations/src/com/courtney/Apple.java
+++ EnumsAutoboxingAnnotations/src/com/courtney/Apple.java
@@ -1,8 +1,13 @@
 package com.courtney;
 
-// Use the built-in enumeration methods.
-
-// An enumeration of apple varieties.
+// Use an enum constructor, instance variable, and method.
 enum Apple {
-    Jonathan, GoldenDel, RedDel, Winesap, Cortland
+    Jonathan(10), GoldenDel(9), RedDel(12), Winesap(15), Cortland(8);
+
+    private int price; // price of each apple
+
+    // Constructor
+    Apple(int p) { price = p; }
+
+    int getPrice() { return price; }
 }
