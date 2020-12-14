b46de98dcce59f1b4d7357bdc9495d01fc452054
CourtneyBritney <courtney02alexander@gmail.com>
2020-12-14 13:31:54 +0200

    Added Files

---
+++ EnumsAutoboxingAnnotations/src/com/courtney/Wrap.java
@@ -0,0 +1,13 @@
+package com.courtney;
+
+// Demonstrate a type wrapper.
+class Wrap {
+    public static void main(String args[]) {
+
+        Integer iOb = new Integer(100);
+
+        int i = iOb.intValue();
+
+        System.out.println(i + " " + iOb); // displays 100 100
+    }
+}
