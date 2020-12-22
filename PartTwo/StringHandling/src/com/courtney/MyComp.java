6b67d039a1dc4455b230540d5dfb14ec79e51dbd
CourtneyBritney <courtney02alexander@gmail.com>
2020-12-22 16:02:48 +0200

    Added Files

---
+++ PartTwo/StringHandling/src/com/courtney/MyComp.java
@@ -0,0 +1,19 @@
+package com.courtney;
+
+// Use a custom comparator.
+import java.util.*;
+
+// A reverse comparator for strings.
+class MyComp implements Comparator<String> {
+    public int compare(String a, String b) {
+        String aStr, bStr;
+
+        aStr = a;
+        bStr = b;
+
+        // Reverse the comparison.
+        return bStr.compareTo(aStr);
+    }
+
+    // No need to override equals.
+} 
