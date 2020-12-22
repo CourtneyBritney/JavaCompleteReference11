0c242670141e63c9fa3178bc0a9c013e68183224
CourtneyBritney <courtney02alexander@gmail.com>
2020-12-22 14:59:47 +0200

    Added Files

--- Modules/src/com/courtney/TestClone.java
+++ Modules/src/com/courtney/TestClone.java
@@ -1,16 +1,16 @@
 package com.courtney;
 
-// Demonstrate the clone() method.
+// Override the clone() method.
 
 class TestClone implements Cloneable {
     int a;
     double b;
 
-    // This method calls Object's clone().
-    TestClone cloneTest() {
+    // clone() is now overridden and is public.
+    public Object clone() {
         try {
             // call clone in Object.
-            return (TestClone) super.clone();
+            return super.clone();
         } catch(CloneNotSupportedException e) {
             System.out.println("Cloning not allowed.");
             return this;
